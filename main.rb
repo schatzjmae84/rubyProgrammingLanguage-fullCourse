ec0652ec6ffab1e59e68853363b8fbd28f3947e3
schatzjmae84 <schatzjm84@gmail.com>
2026-02-25 12:32:23 -0600

    Practiced getting user input. Created a calculator and a Mad Libs game.

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+#
 puts "   /|"
 puts "  / |"
 puts " /  |"
@@ -14,4 +15,34 @@ puts ("However, she did not like being " + character_age + ".")
 phrase = "Giraffe Academy"
 puts phrase.include? "Academy"
 
+num = 20
+puts ("My favorite number is " + num.to_s + ".")
+
+puts 10 / 7
+puts 10 / 7.0
+
+=begin
+puts "Enter Your Name: "
+name = gets.chomp()
+puts ("Hello " + name + "!")
+
+puts "Enter a number: "
+num1 = gets.chomp().to_f
+puts "Enter another number: "
+num2 = gets.chomp().to_f
+
+puts (num1 + num2)
+
+puts "Enter a color: "
+color = gets.chomp()
+puts "Enter a plural noun: "
+plural_noun = gets.chomp()
+puts "Enter a celebrity: "
+celebrity = gets.chomp()
+
+puts ("Roses are " + color)
+puts (plural_noun + " are blue")
+puts ("I love " + celebrity)
+=end
+#
 
