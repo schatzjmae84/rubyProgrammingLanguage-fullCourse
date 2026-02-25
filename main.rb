a1d9d69b57cabe8edebd2b44fe703978faf293de
schatzjmae84 <schatzjm84@gmail.com>
2026-02-25 11:38:02 -0600

    Working on utilizing string methods, variables, and numbers within Ruby.

---
+++ main.rb
@@ -0,0 +1,17 @@
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
+
+character_name = "Unicorny"
+character_age = "23"
+
+puts ("There once was a girl named " + character_name + ".")
+puts ("She was " + character_age + " years old.")
+puts ("She really liked the name " + character_name + ".")
+puts ("However, she did not like being " + character_age + ".")
+
+phrase = "Giraffe Academy"
+puts phrase.include? "Academy"
+
+
