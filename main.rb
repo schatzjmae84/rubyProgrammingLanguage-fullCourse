2686059c926435b54437c7f853f31378687073df
schatzjmae84 <schatzjm84@gmail.com>
2026-02-26 14:00:56 -0600

    Practiced working with the Exponent Method in Ruby, utilizing comments, and how to read files within Ruby that are on our computer.

--- main.rb
+++ main.rb
@@ -193,3 +193,15 @@ end
 6.times do |index|
     puts index
 end
+
+def pow(base_num, pow_num)
+    result = 1
+    pow_num.times do |index|
+        result = result * base_num
+    end
+    return result
+end
+
+puts pow(4, 3)
+
+
